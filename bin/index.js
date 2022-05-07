36bce93de8dafe9bece0b7aa5dc7a02a4ae29dba
rahulyadav139 <ry.rahulmyadav@gmail.com>
2022-05-07 17:59:55 +0530

    docs: accessed all the files in the folder path

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,22 @@
 #! /usr/bin/env node
 
-console.log(__dirname);
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = process.env.npm_config_path;
+
+const allFilePathsData = [];
+
+const getAllFilePaths = directory => {
+  fs.readdirSync(directory).forEach(subDirectory => {
+    if (!subDirectory.startsWith('.')) {
+      const absolutePath = path.join(directory, subDirectory);
+
+      if (fs.statSync(absolutePath).isDirectory())
+        return getAllFilePaths(absolutePath);
+      else return allFilePathsData.push(absolutePath);
+    }
+  });
+};
+
+getAllFilePaths(folderPath);
